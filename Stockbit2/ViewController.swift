c765303e5ce3c97d275785035e817861db2702c9
m.syafrizal@geniebook.com <m.syafrizal@geniebook.com>
2021-12-29 17:45:29 +0700

    init project

---
+++ Stockbit2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Stockbit2
+//
+//  Created by Engineering on 29/12/21.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
